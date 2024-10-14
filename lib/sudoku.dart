909cc0fe72af59e609206fe1dae0f08b60b10295
Armando Silvera <asilvera@nuvotechcolombia.com>
2024-10-14 00:02:38 -0500

    Funcionando!!

--- lib/sudoku.dart
+++ lib/sudoku.dart
@@ -280,11 +280,11 @@ class SudokuState extends State<Sudoku> {
                     });
                   },
                   child: Container(
-                    width: 36,
-                    height: 36,
-                    padding: const EdgeInsets.fromLTRB(12, 4, 5, 5),
+                    width: 30,
+                    height: 30,
+                    padding: const EdgeInsets.fromLTRB(9, 2, 5, 5),
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
+                      borderRadius: BorderRadius.circular(15),
                       color: boton_activo == k ? Colors.blue : Colors.green,
                     ),
                     child: Text(
@@ -335,14 +335,15 @@ class SudokuState extends State<Sudoku> {
     while (!tablero_ok) {
       intentos++;
       print("intentos: $intentos");
-      generarTablero();
+      generaDiagonal();
       setState(() {
-        tablero_ok = validarSolucion();
+        tablero_ok = resolverSudoku();
       });
-      if (intentos > 1000) {
+      if (intentos == 100) {
         setState(() {
           tablero_ok = true;
         });
+        break;
       }
     }
     print("total intentos: $intentos");
@@ -449,7 +450,7 @@ class SudokuState extends State<Sudoku> {
     });
   }
 
-  void generarTablero() {
+  void generaDiagonal() {
     List<int> base = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     List<int> q1 = [0, 1, 2, 10, 11, 12, 20, 21, 22];
     List<int> q2 = [3, 4, 5, 13, 14, 15, 23, 24, 25];
@@ -462,329 +463,106 @@ class SudokuState extends State<Sudoku> {
     List<int> q9 = [66, 67, 68, 76, 77, 78, 86, 87, 88];
     Random random = Random();
 
-    for (var num in base) {
-      try {
-        List<int> q1i = List.from(q1);
-        List<int> q2i = List.from(q2);
-        List<int> q3i = List.from(q3);
-        List<int> q4i = List.from(q4);
-        List<int> q5i = List.from(q5);
-        List<int> q6i = List.from(q6);
-        List<int> q7i = List.from(q7);
-        List<int> q8i = List.from(q8);
-        List<int> q9i = List.from(q9);
-        int randomNumber1 = random.nextInt(q1i.length); // from 0 upto 9
-        int check1 = q1i[randomNumber1];
-        numeros[check1] = num;
-        q1.remove(check1);
-        //quita las opcione de q2 y q3
-        if (check1 < 10) {
-          q2i.remove(3);
-          q2i.remove(4);
-          q2i.remove(5);
-          q3i.remove(6);
-          q3i.remove(7);
-          q3i.remove(8);
-        } else if (check1 < 20) {
-          q2i.remove(13);
-          q2i.remove(14);
-          q2i.remove(15);
-          q3i.remove(16);
-          q3i.remove(17);
-          q3i.remove(18);
-        } else {
-          q2i.remove(23);
-          q2i.remove(24);
-          q2i.remove(25);
-          q3i.remove(26);
-          q3i.remove(27);
-          q3i.remove(28);
-        }
-
-        //quita las opciones de q4 y q7
-        if ([0, 10, 20].contains(check1)) {
-          q4i.remove(30);
-          q4i.remove(40);
-          q4i.remove(50);
-          q7i.remove(60);
-          q7i.remove(70);
-          q7i.remove(80);
-        } else if ([1, 11, 21].contains(check1)) {
-          q4i.remove(31);
-          q4i.remove(41);
-          q4i.remove(51);
-          q7i.remove(61);
-          q7i.remove(71);
-          q7i.remove(81);
-        } else {
-          q4i.remove(32);
-          q4i.remove(42);
-          q4i.remove(52);
-          q7i.remove(62);
-          q7i.remove(72);
-          q7i.remove(82);
-        }
-        int randomNumber2 = random.nextInt(q2i.length);
-        int check2 = q2i[randomNumber2];
-        numeros[check2] = num;
-        q2.remove(check2);
-
-        //quita las opciones de q3
-        if (check2 < 10) {
-          q3i.remove(6);
-          q3i.remove(7);
-          q3i.remove(8);
-        } else if (check2 < 20) {
-          q3i.remove(16);
-          q3i.remove(17);
-          q3i.remove(18);
-        } else {
-          q3i.remove(26);
-          q3i.remove(27);
-          q3i.remove(28);
-        }
-
-        //quita las opciones de q6 y q8
-        if ([3, 13, 23].contains(check2)) {
-          q5i.remove(33);
-          q5i.remove(43);
-          q5i.remove(53);
-          q8i.remove(63);
-          q8i.remove(73);
-          q8i.remove(83);
-        } else if ([4, 14, 24].contains(check2)) {
-          q5i.remove(34);
-          q5i.remove(44);
-          q5i.remove(54);
-          q8i.remove(64);
-          q8i.remove(74);
-          q8i.remove(84);
-        } else {
-          q5i.remove(35);
-          q5i.remove(45);
-          q5i.remove(55);
-          q8i.remove(65);
-          q8i.remove(75);
-          q8i.remove(85);
-        }
-        int randomNumber3 = random.nextInt(q3i.length);
-        int check3 = q3i[randomNumber3];
-        numeros[check3] = num;
-        q3.remove(check3);
-        if ([6, 16, 26].contains(check3)) {
-          q6i.remove(36);
-          q6i.remove(46);
-          q6i.remove(56);
-          q9i.remove(66);
-          q9i.remove(76);
-          q9i.remove(86);
-        } else if ([7, 17, 27].contains(check3)) {
-          q6i.remove(37);
-          q6i.remove(47);
-          q6i.remove(57);
-          q9i.remove(67);
-          q9i.remove(77);
-          q9i.remove(87);
-        } else {
-          q6i.remove(38);
-          q6i.remove(48);
-          q6i.remove(58);
-          q9i.remove(68);
-          q9i.remove(78);
-          q9i.remove(88);
-        }
-        int randomNumber4 = random.nextInt(q4i.length);
-        int check4 = q4i[randomNumber4];
-        numeros[check4] = num;
-        q4.remove(check4);
-        if (check4 < 40) {
-          q5i.remove(33);
-          q5i.remove(34);
-          q5i.remove(35);
-          q6i.remove(36);
-          q6i.remove(37);
-          q6i.remove(38);
-        } else if (check4 < 50) {
-          q5i.remove(43);
-          q5i.remove(44);
-          q5i.remove(45);
-          q6i.remove(46);
-          q6i.remove(47);
-          q6i.remove(48);
-        } else {
-          q5i.remove(53);
-          q5i.remove(54);
-          q5i.remove(55);
-          q6i.remove(56);
-          q6i.remove(57);
-          q6i.remove(58);
-        }
-        if ([30, 40, 50].contains(check4)) {
-          q7i.remove(60);
-          q7i.remove(70);
-          q7i.remove(80);
-        } else if ([31, 41, 51].contains(check4)) {
-          q7i.remove(61);
-          q7i.remove(71);
-          q7i.remove(81);
-        } else {
-          q7i.remove(62);
-          q7i.remove(72);
-          q7i.remove(82);
-        }
-        int randomNumber5 = random.nextInt(q5i.length);
-        int check5 = q5i[randomNumber5];
-        numeros[check5] = num;
-        q5.remove(check5);
-        if (check5 < 40) {
-          q6i.remove(36);
-          q6i.remove(37);
-          q6i.remove(38);
-        } else if (check5 < 50) {
-          q6i.remove(46);
-          q6i.remove(47);
-          q6i.remove(48);
-        } else {
-          q6i.remove(56);
-          q6i.remove(57);
-          q6i.remove(58);
-        }
-        if ([33, 43, 53].contains(check5)) {
-          q8i.remove(63);
-          q8i.remove(73);
-          q8i.remove(83);
-        } else if ([34, 44, 54].contains(check5)) {
-          q8i.remove(64);
-          q8i.remove(74);
-          q8i.remove(84);
-        } else {
-          q8i.remove(65);
-          q8i.remove(75);
-          q8i.remove(85);
-        }
-        int randomNumber6 = random.nextInt(q6i.length);
-        int check6 = q6i[randomNumber6];
-        numeros[check6] = num;
-        q6.remove(check6);
-        if ([36, 46, 56].contains(check6)) {
-          q9i.remove(66);
-          q9i.remove(76);
-          q9i.remove(86);
-        } else if ([37, 47, 57].contains(check6)) {
-          q9i.remove(67);
-          q9i.remove(77);
-          q9i.remove(87);
-        } else {
-          q9i.remove(68);
-          q9i.remove(78);
-          q9i.remove(88);
-        }
-        int randomNumber7 = random.nextInt(q7i.length);
-        int check7 = q7i[randomNumber7];
-        numeros[check7] = num;
-        q7.remove(check7);
-        if (check7 < 70) {
-          q8i.remove(63);
-          q8i.remove(64);
-          q8i.remove(65);
-          q9i.remove(66);
-          q9i.remove(67);
-          q9i.remove(68);
-        } else if (check7 < 80) {
-          q8i.remove(73);
-          q8i.remove(74);
-          q8i.remove(75);
-          q9i.remove(76);
-          q9i.remove(77);
-          q9i.remove(78);
-        } else {
-          q8i.remove(83);
-          q8i.remove(84);
-          q8i.remove(85);
-          q9i.remove(86);
-          q9i.remove(87);
-          q9i.remove(88);
-        }
-        int randomNumber8 = random.nextInt(q8i.length);
-        int check8 = q8i[randomNumber8];
-        numeros[check8] = num;
-        q8.remove(check8);
-        if (check8 < 70) {
-          q9i.remove(66);
-          q9i.remove(67);
-          q9i.remove(68);
-        } else if (check8 < 80) {
-          q9i.remove(76);
-          q9i.remove(77);
-          q9i.remove(78);
-        } else {
-          q9i.remove(86);
-          q9i.remove(87);
-          q9i.remove(88);
-        }
-        numeros[q9i[0]] = num;
-        q9.remove(q9i[0]);
-      } catch (e) {}
+    //genera aleatorio de los 3 cuadrantes de la diagonal
+    q1.shuffle();
+    q5.shuffle();
+    q9.shuffle();
+    //llena la matrix de numeros con los valores aleatorios de q1, q5 y q9
+    for (int i = 1; i < 10; i++) {
+      numeros[q1[i - 1]] = i;
+      numeros[q5[i - 1]] = i;
+      numeros[q9[i - 1]] = i;
     }
+
+    //llena el resto con 0 para que se pueda mostrar
+    for (int i = 1; i < 10; i++) {
+      numeros[q2[i - 1]] = 0;
+      numeros[q3[i - 1]] = 0;
+      numeros[q4[i - 1]] = 0;
+      numeros[q6[i - 1]] = 0;
+      numeros[q7[i - 1]] = 0;
+      numeros[q8[i - 1]] = 0;
+    }
+    print(numeros);
   }
 
-  bool validarSolucion() {
-    bool ok = true;
-    //revisa filas
+  bool resolverSudoku() {
     for (int i = 0; i < 9; i++) {
-      List<int> check_row = [];
       for (int j = 0; j < 9; j++) {
-        if (numeros[i * 10 + j] != null) {
-          check_row.add(numeros[i * 10 + j]!);
-        }
-      }
-      var distinctIds = check_row.toSet().toList();
-      //compara la longitud, si hay un repetido la longitud es diferente
-      print(check_row);
-      print(distinctIds);
-      if (check_row.length != distinctIds.length) {
-        ok = false;
-      }
-    }
-
-    if (ok) {
-      //revisa columnas
-      for (int i = 0; i < 9; i++) {
-        List<int> check_col = [];
-        for (int j = 0; j < 9; j++) {
-          if (numeros[i + j * 10] != null) {
-            check_col.add(numeros[i + j * 10]!);
+        if (numeros[i * 10 + j] == 0) {
+          for (int k = 1; k <= 9; k++) {
+            if (validaFila(i, k) &&
+                validaColumna(j, k) &&
+                validarCuadrante(i, j, k)) {
+              numeros[i * 10 + j] = k;
+              if (resolverSudoku()) {
+                print(numeros);
+                return true;
+              }
+              numeros[i * 10 + j] = 0;
+            }
           }
-        }
-        var distinctIds = check_col.toSet().toList();
-        //compara la longitud, si hay un repetido la longitud es diferente
-        if (check_col.length != distinctIds.length) {
-          print(check_col);
-          print(distinctIds);
-          ok = false;
+          print(numeros);
+          return false;
         }
       }
     }
+    print(numeros);
+    return true;
+  }
 
-    numeros.remove(0);
-    if (numeros.length < 81) {
-      ok = false;
+  bool validaFila(int fila, int valor) {
+    for (int j = 0; j < 9; j++) {
+      if (numeros[fila * 10 + j] == valor) {
+        return false;
+      }
     }
-    return ok;
+    return true;
+  }
+
+  bool validaColumna(int col, int valor) {
+    for (int j = 0; j < 9; j++) {
+      if (numeros[j * 10 + col] == valor) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool validarCuadrante(int fila, int col, int valor) {
+    int posI = subCuadranteActual(fila);
+    int posJ = subCuadranteActual(col);
+    for (int k = posI - 3; k < posI; k++) {
+      for (int l = posJ - 3; l < posJ; l++) {
+        if (numeros[k * 10 + l] == valor) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  int subCuadranteActual(int pos) {
+    if (pos < 3) {
+      return 3;
+    } else if (pos < 6) {
+      return 6;
+    } else {
+      return 9;
+    }
   }
 
   void checkCompleto() {
     print('validnado...');
-    if (validarSolucion()) {
-      print('validacion ok');
-      setState(() {
-        terminado = true;
-      });
-    } else {
-      print('faltan campos');
-      setState(() {
-        terminado = false;
-      });
+    for (int i in numeros.keys) {
+      if (numeros[i] == 0) {
+        setState(() {
+          terminado = false;
+        });
+      }
     }
+    setState(() {
+      terminado = true;
+    });
   }
 }
